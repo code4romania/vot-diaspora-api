61ae3b58209ca56d66472d584a39951f30da82bd
Utwo <mihai.legat@gmail.com>
2020-10-07 19:28:45 +0300

    Remove presentation app

--- src/ClientApp/src/app/map-note/map.note.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-map-note',
-  templateUrl: './map.note.component.html',
-  styleUrls: ['./map.note.component.scss'],
-})
-export class MapNoteComponent {
-}
